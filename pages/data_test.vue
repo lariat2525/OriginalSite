8424d00560ec8e123a63d7120004187ec0433262
liriat <cod4ndss@yahoo.co.jp>
2022-01-21 23:35:29 +0900

    データベース連携したー

---
+++ pages/data_test.vue
@@ -0,0 +1,12 @@
+<template>
+  <h5>ID：{{ items }}</h5>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const items = await $axios.$get("http://localhost:5000");
+    return { items };
+  },
+};
+</script>
