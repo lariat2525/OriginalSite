0ab357cd63a2b95f162738bf6f1a2ec31f296e24
DESKTOP-7ICBPAN\yahir <yahiro0628@outlook.jp>
2022-01-23 03:43:37 +0900

    浦さんへ

--- components/Body/CountryBar.vue
+++ components/Body/CountryBar.vue
@@ -2,4 +2,10 @@
   <div>aaaaaaaaaaaaaaaadhagagasa</div>
 </template>
 <script>
+import CountryData from "~/components/Data/nisiyama_data";
+console.log(CountryData);
+
+  var NisiyamaCountryData= {
+    props: CountryData,
+}
 </script>
