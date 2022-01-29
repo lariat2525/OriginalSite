3c207aa434d9ac68e62f0a9a9b831ded332d46a8
DESKTOP-7ICBPAN\yahir <yahiro0628@outlook.jp>
2022-01-29 16:12:50 +0900

    サンダーさんへ

--- client/components/Body/CountryBar.vue
+++ client/components/Body/CountryBar.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>{{country_data}}</div>
+  <div id="app">
+  <div v-for="continent in computedContinents" :key="continent">
+    {{continent.name}}
+    <ul>
+      <li  v-for="country in continent.countries" :key="country">
+      {{country}}
+      </li>
+    </ul>
+  </div>
+  </div>
+
 </template>
 <script>
-import { country_data } from "./nisiyama_data.js";
+import { country_data } from "~/components/nisiyama_data.js";
 console.log(country_data);
 //配列そのまま表示してるだけだから注意
 //function ここで呼ぶ
@@ -12,7 +22,18 @@ export default {
     return {
       country_data
     };
+  },
+computed:{
+  computedPrice(){
+    const continents = this.countriy_data.continent.map((continentValue, continentIndex)=>{
+      const countries = this.countriy_data.country.reducer((prev, current)=>{
+        return current.continent_id = continentIndex? prev.push(current.name) :prev
+      }, [])
+      return {name: continentValue.name, countries:countries}
+    })
+    return continents
   }
+}
 };
 //この下でfunction定義？？
 </script>
--- client/components/Body/nisiyama_data.js
+++ client/components/Body/nisiyama_data.js
@@ -1,13 +1,13 @@
 export const country_data = {
   country: [
-    { id: 1, country_name: "アメリカ", continent_id: 1 },
-    { id: 2, country_name: "日本", continent_id: 2 },
-    { id: 3, country_name: "イタリア", continent_id: 6 },
-    { id: 4, country_name: "オーストラリア", continent_id: 3 },
-    { id: 5, country_name: "エジプト", continent_id: 4 },
-    { id: 6, country_name: "中国", continent_id: 2 },
-    { id: 7, country_name: "サウジアラビア", continent_id: 2 },
-    { id: 8, country_name: "カナダ", continent_id: 1 },
+    { id: 1, name: "アメリカ", continent_id: 1 },
+    { id: 2, name: "日本", continent_id: 2 },
+    { id: 3, name: "イタリア", continent_id: 6 },
+    { id: 4, name: "オーストラリア", continent_id: 3 },
+    { id: 5, name: "エジプト", continent_id: 4 },
+    { id: 6, name: "中国", continent_id: 2 },
+    { id: 7, name: "サウジアラビア", continent_id: 2 },
+    { id: 8, name: "カナダ", continent_id: 1 },
   ],
   continent: [
     { id: 1, name: "北アメリカ" },
@@ -22,14 +22,14 @@ console.log(country_data);
 // contry_type1:北アメリカ,contry_type2:アジア,contry_type3:ヨーロッパ,contry_type4:オセアニア,contry_type5:アフリカ,contry_type6:南アメリカ　country_bar
 
 // country: [
-//   { country_name: "アメリカ", country_type: 1 },
-//   { country_name: "日本", country_type: 2 },
-//   { country_name: "イタリア", country_type: 3 },
-//   { country_name: "オーストラリア", country_type: 4 },
-//   { country_name: "エジプト", country_type: 5 },
-//   { country_name: "中国", country_type: 2 },
-//   { country_name: "サウジアラビア", country_type: 2 },
-//   { country_name: "カナダ", country_type: 1 }
+//   { name: "アメリカ", country_type: 1 },
+//   { name: "日本", country_type: 2 },
+//   { name: "イタリア", country_type: 3 },
+//   { name: "オーストラリア", country_type: 4 },
+//   { name: "エジプト", country_type: 5 },
+//   { name: "中国", country_type: 2 },
+//   { name: "サウジアラビア", country_type: 2 },
+//   { name: "カナダ", country_type: 1 }
 // ],
 // type: [
 //   "北アメリカ",
